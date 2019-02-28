a6e182472a4da8f2ae08eec7ae006f2a5b913f56
tanshiyang <tanshiyang.cn@gmail.com>
2019-02-28 17:10:57 +0800

    优化

--- stock_analyze_01.py
+++ stock_analyze_01.py
@@ -7,7 +7,7 @@ import tradeday
 pro = pro = ts.pro_api("0577694ff6087849a141deb1c12ddf8566710906b8f64548f03183ce")
 
 
-def every_date(start_date, end_date):
+def every_date(start_date, end_date, table_name):
     # 获取所有有股票
     stock_info = ts.get_stock_basics()
     # 连接数据库
@@ -15,14 +15,21 @@ def every_date(start_date, end_date):
     cursor = conn.cursor()
     codes = stock_info.index
 
-    cursor.execute("drop table if exists stock_analyze_1701;")
+    cursor.execute("drop table if exists %s;" % table_name)
     cursor.execute(
-        "create table stock_analyze_1701 (ts_code varchar(32),start_date varchar(32),end_date varchar(32),close1 float, close2 float,total_revenue float, "
-        "accounts_receiv	float, "    #应收帐款 
-        "n_income	float, "            #利润
-        "adv_receipts	float, "        #预收帐款
+        "create table  %s "
+        "(ts_code varchar(32),"
+        "start_date varchar(32),"
+        "end_date varchar(32),"
+        "close1 float, close2 float,"
+        "total_revenue float, "
+        "accounts_receiv	float, "    # 应收帐款 
+        "n_income	float, "            # 利润
+        "adv_receipts	float, "        # 预收帐款
+        "roe float,"
+        "grossprofit_margin float,"
         "pe float,"
-        "unique(ts_code))")
+        "unique(ts_code))" % table_name)
 
     # 通过for循环以及获取A股只数来遍历每一只股票
     for x in range(0, len(stock_info)):
@@ -33,7 +40,7 @@ def every_date(start_date, end_date):
                 sql = ""
                 while times <= 5 and sql == "":
                     times = times + 1
-                    sql = one_stock(codes[x], start_date, end_date, times)
+                    sql = one_stock(codes[x], start_date, end_date, times, table_name)
 
                 if sql == "" or sql.__contains__("None"):
                     print('%s的数据异常' % codes[x])
@@ -47,7 +54,7 @@ def every_date(start_date, end_date):
     cursor.close()
 
 
-def one_stock(code, start_date, end_date, times):
+def one_stock(code, start_date, end_date, times, table_name):
     if times > 5:
         return ""
     try:
@@ -61,6 +68,10 @@ def one_stock(code, start_date, end_date, times):
         end_date = mydate.string_to_next_quarter(start_date)
         end_date = get_prev_tradeday(end_date)
         print("start:%s,end:%s" % (start_date, end_date))
+
+        fina_indicator = pro.fina_indicator(ts_code=ts_code, start_date=start_date, end_date=end_date)
+        roe = fina_indicator.roe[0]                     # 净资产收益率
+        grossprofit_margin = fina_indicator.grossprofit_margin[0]   # 毛利率
 
         income = pro.income(ts_code=ts_code, start_date=start_date, end_date=end_date)
         total_revenue = income.total_revenue[0]  # 如取最早：len(income) - 1
@@ -76,9 +87,9 @@ def one_stock(code, start_date, end_date, times):
         pe = daily_basic.pe[0]
         print("pe:%s" % pe)
 
-        sql = "insert into stock_analyze_1701 (ts_code,start_date,end_date,close1, close2,total_revenue, n_income, adv_receipts,accounts_receiv,pe) values('%s'," \
-              "'%s','%s','%s','%s','%s','%s','%s','%s','%s') " % (
-                  ts_code, start_date, end_date, close1, close2, total_revenue, n_income, adv_receipts,accounts_receiv, pe)
+        sql = "insert into %s (ts_code,start_date,end_date,close1, close2,total_revenue, n_income, adv_receipts," \
+              "accounts_receiv,roe,grossprofit_margin,pe) values('%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s','%s') " % (
+                  table_name, ts_code, start_date, end_date, close1, close2, total_revenue, n_income, adv_receipts,accounts_receiv,roe,grossprofit_margin, pe)
 
         # print(sql)
         time.sleep(0.3)
@@ -99,5 +110,6 @@ def get_prev_tradeday(date):
     return date
 
 
-every_date('20170101', '20180301')
-# print(one_stock("000540", '20180102', '20181231', 1))
+every_date('20180701', '20181001','stock_analyze_1803')
+every_date('20181001', '20190101','stock_analyze_1804')
+
