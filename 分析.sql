f7e5b8263d8a9c13e86a702460c5b7418e8a64bc
tanshiyang <tanshiyang.cn@gmail.com>
2019-03-04 21:24:27 +0800

    优化2

--- 分析.sql
+++ 分析.sql
@@ -9,13 +9,15 @@ SELECT
 	( if(close_max>close1,close_max,close2) - close1 ) / close1 * 100 AS up_rate,
 	roe / grossprofit_margin  roe_gross,
 	adv_receipts / total_revenue AS rec_rev,
+	accounts_receiv / total_revenue AS acc_rev,
 	n_income / total_revenue AS inc_rev ,
-	(abs(roe) * (adv_receipts / total_revenue) )  as order1
+	(abs(roe) * (adv_receipts / total_revenue)) as order1
 FROM
 	stock_analyze_1803 a
 	) a
 WHERE
 	1 = 1
+	and total_revenue>accounts_receiv
 	and roe < 1 and roe > -100
 	and pe <65
 ORDER BY
