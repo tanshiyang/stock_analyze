6be9c3357941fcec7b925a8e2fd0f27473e45aab
tanshiyang <tanshiyang.cn@gmail.com>
2019-03-15 16:14:57 +0800

    优化

--- mydb.py
+++ mydb.py
@@ -2,4 +2,4 @@ import mysql.connector
 
 
 def conn():
-    return mysql.connector.connect(user='root', password='123123', database='mystock')
+    return mysql.connector.connect(user='root', password='123123', database='mystock', auth_plugin='mysql_native_password')
