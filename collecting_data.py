9e7e07e9f30bb849272cca97e1aa0ce606739d54
tanshiyang <tanshiyang.cn@gmail.com>
2019-04-20 09:57:53 +0800

    优化2

--- collecting_data.py
+++ collecting_data.py
@@ -1304,11 +1304,11 @@ if __name__ == '__main__':
 
     period_info = mydate.get_period_info()
     period_date = period_info[0]
-    # price_period.collect_price(period_date)
-    # collect_disclosure(period_date)
+    price_period.collect_price(period_date)
+    collect_disclosure(period_date)
     collect_income(period_date)
     collect_balancesheet(period_date)
-    # collect_fina_indicator(period_date)
+    collect_fina_indicator(period_date)
 
     '''
     for year in range(2015, 2018):
