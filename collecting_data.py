d122235cb2aa2469bcc3f513ebc0ddcdb5e69fd9
tanshiyang <tanshiyang.cn@gmail.com>
2019-04-26 11:37:45 +0800

    优化

--- collecting_data.py
+++ collecting_data.py
@@ -1310,8 +1310,8 @@ if __name__ == '__main__':
     collect_balancesheet(period_date)
     collect_fina_indicator(period_date)
 
-    '''
-    for year in range(2015, 2018):
+    # '''
+    for year in range(2018, 2019):
         for md in ["0331", "0630", "0930", "1231"]:
             period_date = str(year) + md
             price_period.collect_price(period_date)
@@ -1319,4 +1319,4 @@ if __name__ == '__main__':
             collect_income(period_date)
             collect_balancesheet(period_date)
             collect_fina_indicator(period_date)
-    '''
+    # '''
