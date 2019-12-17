0e05ee37983e6494c8820b5236eb687031b1a418
tanshiyang <tanshiyang.cn@gmail.com>
2019-12-17 08:58:32 +0800

    优化

--- mytusharepro.py
+++ mytusharepro.py
@@ -6,7 +6,8 @@ class MyTusharePro:
     def __init__(self):
         ts.set_token("0577694ff6087849a141deb1c12ddf8566710906b8f64548f03183ce")
         self.pro = ts.pro_api("0577694ff6087849a141deb1c12ddf8566710906b8f64548f03183ce")
-        self.max_call_times = 999999
+        self.max_call_times = 2
+        self.sleep_time = 20
 
     def disclosure_date(self, ts_code=None, end_date=None, pre_date=None, actual_date=None, times=1):
         if times > self.max_call_times:
@@ -16,8 +17,8 @@ class MyTusharePro:
                                             actual_date=actual_date)
         except Exception as e:
             print(e)
-            print("休息 30s ")
-            time.sleep(30)
+            print(str.format("休息{0}s", self.sleep_time))
+            time.sleep(self.sleep_time)
             return self.disclosure_date(ts_code, end_date, pre_date, actual_date, times + 1)
         finally:
             time.sleep(0.3)
@@ -32,8 +33,8 @@ class MyTusharePro:
                                    )
         except Exception as e:
             print(e)
-            print("休息 30s ")
-            time.sleep(30)
+            print(str.format("休息{0}s", self.sleep_time))
+            time.sleep(self.sleep_time)
             return self.income(ts_code, ann_date, start_date, end_date, period, report_type, comp_type, times + 1)
         finally:
             time.sleep(0.3)
@@ -48,8 +49,8 @@ class MyTusharePro:
                                          )
         except Exception as e:
             print(e)
-            print("休息 30s ")
-            time.sleep(30)
+            print(str.format("休息{0}s", self.sleep_time))
+            time.sleep(self.sleep_time)
             return self.balancesheet(ts_code=ts_code, ann_date=ann_date, start_date=start_date, end_date=end_date,
                                      period=period, report_type=report_type, comp_type=comp_type, times=times + 1)
         finally:
@@ -64,8 +65,8 @@ class MyTusharePro:
                                            )
         except Exception as e:
             print(e)
-            print("休息 30s ")
-            time.sleep(30)
+            print(str.format("休息{0}s", self.sleep_time))
+            time.sleep(self.sleep_time)
             return self.fina_indicator(ts_code, ann_date, start_date, end_date, period, times + 1)
         finally:
             time.sleep(0.3)
@@ -78,8 +79,8 @@ class MyTusharePro:
             return self.pro.daily(ts_code=ts_code, trade_date=trade_date, start_date=start_date, end_date=end_date)
         except Exception as e:
             print(e)
-            print("休息 30s ")
-            time.sleep(30)
+            print(str.format("休息{0}s", self.sleep_time))
+            time.sleep(self.sleep_time)
             return self.daily(ts_code, trade_date, start_date, end_date, times + 1)
         finally:
             time.sleep(0.3)
@@ -92,8 +93,8 @@ class MyTusharePro:
             return self.pro.daily_basic(ts_code=ts_code, trade_date=trade_date, start_date=start_date, end_date=end_date, fields=fields)
         except Exception as e:
             print(e)
-            print("休息 30s ")
-            time.sleep(30)
+            print(str.format("休息{0}s", self.sleep_time))
+            time.sleep(self.sleep_time)
             return self.daily_basic(ts_code, trade_date, start_date, end_date, fields, times + 1)
         finally:
             time.sleep(0.3)
@@ -106,8 +107,8 @@ class MyTusharePro:
             return self.pro.trade_cal(exchange=exchange, start_date=start_date, end_date=end_date, is_open=is_open)
         except Exception as e:
             print(e)
-            print("休息 30s ")
-            time.sleep(30)
+            print(str.format("休息{0}s", self.sleep_time))
+            time.sleep(self.sleep_time)
             return self.trade_cal(exchange, start_date, end_date, is_open, times + 1)
         finally:
             time.sleep(0.3)
@@ -116,11 +117,13 @@ class MyTusharePro:
         if times > self.max_call_times:
             raise Exception("尝试调用Tushare超出最大次数!", 1)
         try:
-            return ts.pro_bar(ts_code=ts_code,start_date=start_date,end_date=end_date,adj=adj)
+            result = ts.pro_bar(ts_code=ts_code,start_date=start_date,end_date=end_date,adj=adj)
+            if result is None:
+                raise Exception("结果为None.")
         except Exception as e:
             print(e)
-            print("休息 30s ")
-            time.sleep(30)
+            print(str.format("休息{0}s", self.sleep_time))
+            time.sleep(self.sleep_time)
             return self.pro_bar(ts_code=ts_code,start_date=start_date,end_date=end_date,adj=adj, times=times + 1)
         finally:
             time.sleep(0.3)
