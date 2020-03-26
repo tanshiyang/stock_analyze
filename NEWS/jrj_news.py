f678270b35ee1a99e8737395b183476d326f0e07
tanshiyang <tanshiyang.cn@gmail.com>
2020-03-26 11:56:00 +0800

    优化

---
+++ NEWS/jrj_news.py
@@ -0,0 +1,58 @@
+import urllib
+import re
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+import time
+
+
+class JRJNews:
+
+    def get_html(self, url):
+        try:
+            html = urllib.request.urlopen(url).read()
+            html = html.decode('gbk')
+            return html
+        except Exception as e:
+            return ""
+
+    def get_news(self, start_date, end_date):
+        result_df = pd.DataFrame()
+        ssgs = self.get_ssgs_news(start_date,end_date)
+        result_df = pd.concat([result_df, ssgs])
+        return result_df
+
+    def get_ssgs_news(self, start_date, end_date):
+        result_df = pd.DataFrame()
+        start_time = time.strptime(start_date, "%Y-%m-%d %H:%M:%S")
+        end_time = time.strptime(end_date, "%Y-%m-%d %H:%M:%S")
+        pages = ["", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+        for page in pages:
+            url = time.strftime('http://stock.jrj.com.cn/xwk/%Y%m/%Y%m%d_{0}.shtml'.format(page))
+            if page != "":
+                page = "-" + page
+            url = time.strftime('http://stock.jrj.com.cn/list/stockssgs{0}.shtml'.format(page))
+            html = self.get_html(url)
+            if html == '':
+                break
+            soup = BeautifulSoup(html, 'html.parser')
+            ul = soup.select('div[class="list-main"] > ul > li')
+            for child in ul:
+                if len(child.select("a")) == 0:
+                    continue
+                news = {}
+                news["content"] = news["title"] = child.select("a")[0].getText()
+                news["datetime"] = child.select("span")[0].getText()
+                news["channels"] = ""
+                # print(news)
+                if news["title"] == "":
+                    continue
+                if start_date <= news["datetime"] <= end_date:
+                    result_df = result_df.append(news, ignore_index=True)
+        return result_df
+
+if __name__ == '__main__':
+    jrj = JRJNews()
+    while True:
+        jrj.get_news('2019-01-17 20:58:32', '2022-01-17 20:58:32')
+        time.sleep(5)
--- NEWS/news_monitor.py
+++ NEWS/news_monitor.py
@@ -11,6 +11,8 @@ import pandas as pd
 import mytusharepro
 import my_email.sendmail as sendmail
 from collections import deque
+import NEWS.jrj_news as jrj
+
 
 pro = mytusharepro.MyTusharePro()
 curPath = os.path.abspath(os.path.dirname(__file__))
@@ -20,59 +22,20 @@ sys.path.append(rootPath)
 data_path = os.path.join(curPath, 'data')
 
 
-def monitor(file_name):
-    recent_news_deque = deque(maxlen=20)
-    last_news_time = get_last_news_time()
-    last_save_time = time.perf_counter()
-    news_src = {'sina': last_news_time, 'wallstreetcn': last_news_time,
-                '10jqka': last_news_time, 'eastmoney': last_news_time,
-                'yuncaijing': last_news_time}
-    result_df = pd.DataFrame(columns=["date_time", "keywords", "content", "src"])
+def my_decode(keyword):
+    return keyword.replace("%2C", ",")
 
-    while True:
-        focus_keyword_df = pd.read_csv(file_name, encoding="gbk")
-        for src in news_src.keys():
-            start_date = news_src[src]
-            end_date = time.strftime('%Y-%m-%d %H:%M:%S')
-            print("news: {0}，{1},{2}".format(start_date, end_date, src))
-            news_df = pro.news(src=src, start_date=start_date, end_date=end_date)
-            for news_index, news_row in news_df.iterrows():
-                date_time = news_row["datetime"]
-                content = news_row["content"]
 
-                matched_keywords = []
-                for keywords_index, keywords_row in focus_keyword_df.iterrows():
-                    keywords = keywords_row["keywords"]
-                    if check_contains_keywords(content, keywords):
-                        matched_keywords.append(keywords)
-                if len(matched_keywords) > 0:
-                    next_time = datetime.datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S') + \
-                                datetime.timedelta(seconds=1)
-                    if news_src[src] < next_time.strftime('%Y-%m-%d %H:%M:%S'):
-                        news_src[src] = next_time.strftime('%Y-%m-%d %H:%M:%S')
-                    # 判断重复性
-                    if check_duplicate(recent_news_deque, content):
-                        continue
-                    recent_news_deque.append(content)
-                    dict = {}
-                    dict["date_time"] = date_time
-                    dict["keywords"] = matched_keywords
-                    dict["content"] = mark_content(content, matched_keywords)
-                    dict["src"] = src
-                    print(dict)
-                    result_df = result_df.append(dict, ignore_index=True)
+def mark_content_replace(match_obj):
+    return "<b>" + match_obj.group(0) + "</b>"
 
-        today = time.strftime('%Y%m%d')
-        output_file_name = os.path.join(data_path, "result", "focus_news_{0}.csv".format(today))
-        if len(result_df) > 0 and time.perf_counter() - last_save_time > 60:
-            try:
-                save_and_send(result_df, output_file_name)
-                # result_df.to_csv(output_file_name, mode='a', header=False, encoding="utf_8_sig")
-                result_df = pd.DataFrame(columns=["date_time", "keywords", "content", "src"])
-            except Exception as e:
-                print('str(e):\t', str(e))
 
-        time.sleep(30)
+def mark_content(content=str, keywords_list=[]):
+    for keywords in keywords_list:
+        for keyword in keywords.split(","):
+            content = re.sub(my_decode(keyword), mark_content_replace, content)
+            # content = content.replace(keyword, "<b>" + keyword + "</b>")
+    return content
 
 
 def save_and_send(result_df=pd.DataFrame, output_file_name=str):
@@ -83,13 +46,13 @@ def save_and_send(result_df=pd.DataFrame, output_file_name=str):
         mail_content += "内容：{0}<br/>".format(news_row["content"])
         mail_content += "来源：{0}<br/>".format(news_row["src"])
         mail_content += "<p/>"
-    sendmail.send_news_mail(mail_content)
+    # sendmail.send_news_mail(mail_content)
     result_df.to_csv(output_file_name, mode='a', header=False, encoding="utf_8_sig")
 
 
 def check_contains_keywords(content, keywords):
     for keyword in keywords.split(","):
-        if keyword not in content:
+        if not re.search(my_decode(keyword), content):  # keyword not in content:
             return False
     return True
 
@@ -111,21 +74,76 @@ def get_last_news_time():
     return last_time
 
 
-def mark_content(content=str, keywords_list=[]):
-    for keywords in keywords_list:
-        for keyword in keywords.split(","):
-            content = content.replace(keyword, "<b>" + keyword + "</b>")
-    return content
+class NewsMonitor:
+    def __init__(self):
+        self.recent_news_deque = deque(maxlen=20)
+        last_news_time = get_last_news_time()
+        self.last_save_time = time.perf_counter()
+        self.news_src = {'sina': last_news_time, 'wallstreetcn': last_news_time,
+                    '10jqka': last_news_time, 'eastmoney': last_news_time,
+                    'yuncaijing': last_news_time}
+        self.result_df = pd.DataFrame(columns=["date_time", "keywords", "content", "src"])
+        self.jrj_news = jrj.JRJNews()
+        self.focus_keyword_df = pd.DataFrame()
 
+    def monitor(self,file_name):
+        while True:
+            self.focus_keyword_df = pd.read_csv(file_name, encoding="gbk")
+            for src in self.news_src.keys():
+                start_date = self.news_src[src]
+                end_date = time.strftime('%Y-%m-%d %H:%M:%S')
+                print("news: {0}，{1},{2}".format(start_date, end_date, src))
+                news_df = pro.news(src=src, start_date=start_date, end_date=end_date)
+                self.check_news(src,news_df)
 
-def check_duplicate(recent_news_deque: deque, content: str):
-    for recent_news in recent_news_deque:
-        if difflib.SequenceMatcher(None, recent_news, content).quick_ratio() >= 0.8:
-            print("skip:{0}".format(content))
-            return True
-    return False
+            today = time.strftime('%Y%m%d')
+            output_file_name = os.path.join(data_path, "result", "focus_news_{0}.csv".format(today))
+            if len(self.result_df) > 0 and time.perf_counter() - self.last_save_time > 60:
+                try:
+                    save_and_send(self.result_df, output_file_name)
+                    # result_df.to_csv(output_file_name, mode='a', header=False, encoding="utf_8_sig")
+                    result_df = pd.DataFrame(columns=["date_time", "keywords", "content", "src"])
+                except Exception as e:
+                    print('str(e):\t', str(e))
+
+            time.sleep(30)
+
+    def check_news(self, src=str, news_df=pd.DataFrame):
+        for news_index, news_row in news_df.iterrows():
+            date_time = news_row["datetime"]
+            content = news_row["content"]
+
+            matched_keywords = []
+            for keywords_index, keywords_row in self.focus_keyword_df.iterrows():
+                keywords = keywords_row["keywords"]
+                if check_contains_keywords(content, keywords):
+                    matched_keywords.append(keywords)
+            if len(matched_keywords) > 0:
+                next_time = datetime.datetime.strptime(date_time, '%Y-%m-%d %H:%M:%S') + \
+                            datetime.timedelta(seconds=1)
+                if self.news_src[src] < next_time.strftime('%Y-%m-%d %H:%M:%S'):
+                    self.news_src[src] = next_time.strftime('%Y-%m-%d %H:%M:%S')
+                # 判断重复性
+                if self.check_duplicate(content):
+                    continue
+                self.recent_news_deque.append(content)
+                dict = {}
+                dict["date_time"] = date_time
+                dict["keywords"] = matched_keywords
+                dict["content"] = mark_content(content, matched_keywords)
+                dict["src"] = src
+                print(dict)
+                self.result_df = self.result_df.append(dict, ignore_index=True)
+
+    def check_duplicate(self, content: str):
+        for recent_news in self.recent_news_deque:
+            if difflib.SequenceMatcher(None, recent_news, content).quick_ratio() >= 0.8:
+                print("skip:{0}".format(content))
+                return True
+        return False
 
 
 if __name__ == '__main__':
-    monitor(os.path.join(data_path, "news_focus_keywords.csv"))
+    monitor = NewsMonitor()
+    monitor.monitor(os.path.join(data_path, "news_focus_keywords.csv"))
     print()
